c3fce4a2b4305c19cab705bd3fa64a52859f7f3b
Nursultan Bekenov <nursultan.bekenov@t1a.com>
2020-05-10 14:19:47 -0400

    add terraform variables types

---
+++ terraform_aws/simple_intro_example/vars_types.tf
@@ -0,0 +1,34 @@
+variable "my_var" {
+  type    = "string"
+  default = "hellow world"
+}
+# refer to the variables
+# var.my_var
+
+variable "my_map" {
+  type = map(string)
+  default = {
+    mykey = "my value"
+  }
+}
+# refer to the variables
+# var.my_map["mykey"] or "${var.my_map["mykey"]}"
+
+variable "my_lst" {
+  type    = list
+  default = [1, 2, 3]
+}
+# refer to the variables
+# var.my_lst[0]
+# element(var.my_lst, 0)
+# slice(var.my_lst, 0 , 2)
+
+variable "my_bool" {
+  type    = bool
+  default = True
+}
+
+variable "my_num" {
+  type    = number
+  default = 19
+}
