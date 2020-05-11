ade0539681f505e4c9be48b4dff889f2b1679e3c
Nursultan Bekenov <nursultan.bekenov@t1a.com>
2020-05-11 14:11:05 -0400

    add creation of aws key pair

---
+++ terraform_aws/create_aws_key_pair/provider.tf
@@ -0,0 +1,4 @@
+provider "aws" {
+  profile = var.AWS_PROFILE_NAME
+  region  = "us-east-1"
+}
---
+++ terraform_aws/create_aws_key_pair/variables.tf
@@ -0,0 +1,14 @@
+variable "AWS_REGION" {
+  default = "us-east-1"
+}
+
+variable "AWS_PROFILE_NAME" {
+}
+
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "mykey"
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "mykey.pub"
+}
