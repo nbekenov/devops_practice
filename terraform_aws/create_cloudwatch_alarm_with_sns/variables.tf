0be027c78d1886acb45920170c074d8a4b7bdb74
Nursultan Bekenov <nursultan.bekenov@t1a.com>
2020-05-14 14:37:23 -0400

    fix issue with vars

--- terraform_aws/create_cloudwatch_alarm_with_sns/variables.tf
+++ terraform_aws/create_cloudwatch_alarm_with_sns/variables.tf
@@ -1,6 +1,12 @@
 variable "alarms_email" {
-  default = "example@gmail.com"
 }
-variable "my_instance_id" {
-  default = "i-exampleid"
+
+variable "instance_id_lst" {
+  type    = list
+  default = ["i-my_instance_id", "i-my_instance_id2"]
+}
+
+variable "instance_name" {
+  type    = list
+  default = ["server_1", "server_2"]
 }
