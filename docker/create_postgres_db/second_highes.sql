e52ec1c663856bc3d19d36645d468491efe26702
Nursultan Bekenov <nur.beken.usa@gmail.com>
2020-07-17 20:42:00 -0400

    add new task

--- docker/create_postgres_db/second_highes.sql
+++ docker/create_postgres_db/second_highes.sql
@@ -1,4 +1,37 @@
  -- Second Highest Salary
 SELECT MAX(Salary) AS SecondHighestSalary
 FROM Employee
-WHERE Salary < (SELECT MAX(Salary) FROM Employee)
+WHERE Salary < (SELECT MAX(Salary) FROM Employee);
+
+
+
+-- SELECT
+--     (SELECT DISTINCT
+--             Salary
+--         FROM
+--             Employee
+--         ORDER BY Salary DESC
+--         LIMIT 1 OFFSET 1) AS SecondHighestSalary;
+
+-- SELECT
+--     IFNULL(
+--       (SELECT DISTINCT Salary
+--        FROM Employee
+--        ORDER BY Salary DESC
+--         LIMIT 1 OFFSET 1),
+--     NULL) AS SecondHighestSalary;
+
+
+select 
+Department as "Department",Employee as "Employee",Salary as "Salary"
+from (
+    select 
+        emp.name as Employee, 
+        emp.salary as Salary, 
+        dep.Name as Department,
+        ROW_NUMBER() OVER (PARTITION BY dep.Name  ORDER BY emp.salary DESC ) AS ROW_NUM
+    from Employee emp
+    join Department dep
+    on emp.DepartmentId=dep.id
+    )
+where ROW_NUM<=3;
